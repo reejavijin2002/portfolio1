6063a03f37432493fe6e612d18d2dfa22c0e0050
reejavijin2002 <vijincp7@gmail.com>
2023-08-22 15:04:30 +0530

    portfolio1

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ function App() {
   <div>
    <div className="one"  >
 
-   <Navbar className="bg-body-primary"  >
-      <Container  >
+   <Navbar className="bg-body-primary" style={{backgroundColor:"#2bb550"}} >
+      <Container className="yy" >
         <Navbar.Brand href="#home" style={{color:"aliceblue",fontWeight:"bolder",fontStyle:"oblique",marginTop:"0px"}}><h3>About Myself</h3></Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
@@ -33,24 +33,27 @@ function App() {
 
    </div>
    <div className="ef">
+    <div>
    <div className="two">
-<div className="three">
+<div className="three " >
 
-    <h1 style={{textShadow:"3px 1px"}}>Hi! I'm!</h1>
-    <h1 style={{color:"#242624" ,textShadow:"3px 1px"}}>Vijin cp</h1>
+    <h1 className="qq" style={{textShadow:"3px 1px"}}>Hi! I'm!</h1>
+    <h1 style= {{color:"#242624" ,textShadow:"3px 1px"}}>Vijin cp</h1>
     <h1 style={{textShadow:"3px 1px"}}>Mern Stack Developer</h1><br></br>
 
     <p  style={{color:"#242624"}}>I'm a tech enthusiast who thrives on challenges and believes in the power of clean, efficient code to create seamless user experiences. My journey into the world of web development started with a curiosity-driven exploration and has now evolved into a full-fledged career where I continue to learn and grow every day.</p>
-    <button style={{width:'100px',borderRadius:"15px"}}>Hire Me</button>
-    <button style={{width:'100px',borderRadius:"15px",marginLeft:"15px",backgroundColor:"white", color:"#2bb550",border:"1px solid green"}}>Yes Talk</button>
+    <button style={{width:'100px',borderRadius:"15px"}} className="gg">Hire Me</button>
+    <button  className="gg" style={{width:'100px',borderRadius:"15px",marginLeft:"15px",backgroundColor:"white", color:"#2bb550",border:"1px solid green"}}>Yes Talk</button>
 
 </div>
+<div className="zz">
+  <img  src={img1} alt='' height={'376px'} width={'250px'} style={{marginRight:"50px",borderRadius:"100px",border:"none"}}></img>
+</div>
+</div>
+</div>
 <div>
-  <img src={img1} alt='' height={'376px'} width={'250px'} style={{marginRight:"50px",borderRadius:"100px",border:"none"}}></img>
-</div>
-</div>
-<div className="four" style={{display:"flex"}}>
-  <div className="five">
+<div className="four" id="dd" >
+  <div className="five" style={{alignItems:"center"}}  >
 
   <Link to="about"><button style={{marginBottom:"20px",borderRadius:"25px"}}>About</button><br></br></Link>
     <Link to="education"><button style={{marginBottom:"20px",borderRadius:"25px"}}>Education</button><br></br></Link>
@@ -68,6 +71,7 @@ function App() {
   <div className="six shadow p-2 m-4" style={{}} >
     <Outlet />
     
+  </div>
   </div>
 </div>
 
